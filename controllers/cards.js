1ec4a40937a25cb1f6b03e77cd7bad7b92832f00
patteesolomon <patteesolomon@gmail.com>
2023-02-03 00:55:57 -0600

    this

--- controllers/cards.js
+++ controllers/cards.js
@@ -11,33 +11,39 @@ router.get('/', (req, res)=>{
 });
 // New - Will be handled by React application
 // Delete
-router.delete('/:title', (req, res)=>{
+router.delete('/:id', (req, res)=>{
     Cards.findByIdAndRemove(req.params.id, (err, deletedCard)=>{
         res.json(deletedCard);
     });
 });
 // Update
-router.put('/:title', (req, res)=>{
+router.put('/:id', (req, res)=>{
     Cards.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedCard)=>{
         res.json(updatedCard);
     });
 });
+
+
 // Create
 router.post('/', (req, res)=>{
     Cards.create(req.body, (err, createdCard)=>{
         // const picGrab = axios.post('https://api.pexels.com/v1/' +
-        // `${card.title}`);
+        // `${card.id}`);
         // card.Image = picGrab;
         res.json(createdCard); //.json() will send proper headers in response so client knows it's json coming back
     });
 });
 // Edit - Will be handled by React application
 // Show
-router.get('/:title', (req, res)=>{
+router.get('/:id', (req, res)=>{
     Cards.findById(req.params.id, (err, foundCard)=>{
         res.json(foundCard);
     });
 });
 
-
+router.get('/:title', (req, res) =>{
+    Cards.findById(req.params.title, (err, foundCard)=>{
+        res.json(foundCard);
+    });
+});
 module.exports = router;
