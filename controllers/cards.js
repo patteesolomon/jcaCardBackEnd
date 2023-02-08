a000e123b7bd9d853c96ed9244ad67ee2429b3be
patteesolomon <patteesolomon@gmail.com>
2023-02-07 21:41:35 -0600

    re-routing in progress

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Cards = require('../models/cards.js');
 // Remember INDUCES
-
+var isValid = false;
 // Index
-router.get('/', (req, res)=>{
+router.get('/:title', (req, res)=>{
     Cards.find({}, (err, foundCards)=>{
         res.json(foundCards);
     });
@@ -16,6 +16,7 @@ router.delete('/:id', (req, res)=>{
         res.json(deletedCard);
     });
 });
+
 // Update
 router.put('/:id', (req, res)=>{
     Cards.findByIdAndUpdate(req.params.id, req.body, (err, card)=>{
@@ -23,27 +24,41 @@ router.put('/:id', (req, res)=>{
     });
 });
 
-
 // Create
-router.post('/', (req, res)=>{
-    Cards.create(req.body, (err, createdCard)=>{
-        // const picGrab = axios.post('https://api.pexels.com/v1/' +
-        // `${card.id}`);
-        // card.Image = picGrab;
-        res.json(createdCard); //.json() will send proper headers in response so client knows it's json coming back
-    });
+router.post('/title', (req, res) => {
+    if (isValid) {
+        Cards.push({ title: req.body.title });
+        //res.redirect(`/cards/${Cards.length - 1}`);
+    } else {
+        console.log("Error");
+        res.render("cards/new", {title : req.body.title});
+    }
+    
+    
+    // (req.body, (err, card)=>{
+    //     // const picGrab = axios.post('https://api.pexels.com/v1/' +
+    //     // `${card.id}`);
+    //     // card.Image = picGrab;
+    //     res.json(card); //.json() will send proper headers in response so client knows it's json coming back
+    // });
 });
-// Edit - Will be handled by React application
+// Edit - Will be handled by  application
 // Show
 // router.get('/:id', (req, res)=>{
-//     Cards.findById(req.params.id, (err, foundCard)=>{
+//     Cards.findById(req.parReactams.id, (err, foundCard)=>{
 //         res.json(foundCard);
 //     });
 // });
 
 router.get('/:id', (req, res) =>{
     Cards.findById(req.params.title, (err, foundCard)=>{
-        res.json(foundCard);
+        if (foundCard != null) {
+            res.json(foundCard);
+            isValid = true;
+        }
+        else{
+            console.error(err);
+        }
     });
 });
 
